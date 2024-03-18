cf54859e1128d87b289063fc800ae4a37ca59472
tomasrpita <tomasrpita@gmail.com>
2024-03-18 06:52:23 +0100

    Update TailwindCSS configuration and add DashboardLayout component

--- src/presentation/layouts/DashboardLayout.tsx
+++ src/presentation/layouts/DashboardLayout.tsx
@@ -1,10 +1,47 @@
-import { Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom";
+import { menuRoutes } from '../router/router';
 
 export const DashboardLayout = () => {
   return (
-    <>
-    <h1>DashBoard</h1>
-    <Outlet />
-    </>
-  )
-}
+    <main className="flex flex-row mt-7">
+      <nav className="hidden sm:flex flex-col ml-5 w-[370px] min-h-[calc(100vh-3.0rem)] bg-white bg-opacity-10 p-5 rounded-3xl">
+        <h1 className="font-bold text-lg lg:text-3xl bg-gradient-to-br from-white via-white/50 bg-clip-text text-transparent">
+          ReactGPT<span className="text-indigo-500">.</span>
+        </h1>
+        <span className="text-xl">Bienvenido</span>
+
+        <div className="border-gray-700 border my-3" />
+
+        {/* Opciones del menú */}
+        {
+          menuRoutes.map(option => (
+            <NavLink
+            key={option.to}
+            to={option.to}
+            className={({ isActive }) =>
+              isActive
+                ? "flex justify-center items-center bg-gray-800 rounded-md p-2 transition-colors"
+                : "flex justify-center items-center hover:bg-gray-800 rounded-md p-2 transition-colors"
+            }
+          >
+            <i className={`${option.icon} text-2xl mr-4 text-indigo-400`}></i>
+            <div className="flex flex-col flex-grow">
+              <span className="text-white text-lg font-semibold">{option.title}</span>
+              <span className="text-gray-400 text-sm">{option.description}</span>
+            </div>
+          </NavLink>
+          ))
+        }
+        
+      </nav>
+
+      <section className="mx-3 sm:mx-20 flex flex-col w-full h-[calc(100vh-50px)]  bg-white bg-opacity-10 p-5 rounded-3xl">
+        <div className="flex flex-row h-full">
+          <div className="flex flex-col flex-auto h-full p-1">
+            <Outlet />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
