33782facfbd9e706f69399bc3e38a6ef412d3fda
tomasrpita <tomasrpita@gmail.com>
2024-03-19 06:31:26 +0100

    Refactor DashboardLayout component and update menuRoutes in DashboardLayout.tsx

--- src/presentation/layouts/DashboardLayout.tsx
+++ src/presentation/layouts/DashboardLayout.tsx
@@ -1,5 +1,6 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { menuRoutes } from '../router/router';
+import { SidebarMenuItem } from "../components/sidebar/SidebarMenuItem";
 
 export const DashboardLayout = () => {
   return (
@@ -15,21 +16,7 @@ export const DashboardLayout = () => {
         {/* Opciones del menú */}
         {
           menuRoutes.map(option => (
-            <NavLink
-            key={option.to}
-            to={option.to}
-            className={({ isActive }) =>
-              isActive
-                ? "flex justify-center items-center bg-gray-800 rounded-md p-2 transition-colors"
-                : "flex justify-center items-center hover:bg-gray-800 rounded-md p-2 transition-colors"
-            }
-          >
-            <i className={`${option.icon} text-2xl mr-4 text-indigo-400`}></i>
-            <div className="flex flex-col flex-grow">
-              <span className="text-white text-lg font-semibold">{option.title}</span>
-              <span className="text-gray-400 text-sm">{option.description}</span>
-            </div>
-          </NavLink>
+            <SidebarMenuItem {...option} key={option.to} />
           ))
         }
         
